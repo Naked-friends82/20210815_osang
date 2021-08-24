73162d2b96ac6c4f2e48f22932231e71a592172f
Naked_Friends82 <xkwhdnfdmathfl@hanmail.net>
2021-08-24 19:59:01 +0900

    nav bar in mobile

--- src/components/Navig.js
+++ src/components/Navig.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link} from 'react-router-dom'
 import mainLogo from '../asset/mainLogo.png'
 import "../style/web/navig.css";
@@ -6,7 +6,12 @@ import "../style/mobile/navig_mobile.css";
 
 
 const Navig = () => {
-  
+  const [navToggle, setNavToggle] = useState(false);
+
+  const nav_toggle = () => {
+    setNavToggle(!navToggle);
+  }
+
   return(
     <header className="navBox">
       <div className="navBox_1">
@@ -14,15 +19,17 @@ const Navig = () => {
           <img className="mainLogo" src={mainLogo} alt="osangmain Main Logo" />
         </Link>
       </div>
-      <nav className="navBox_2__nav">
-          <Link className="link" to="/">Work</Link>
-          <Link className="link" to="/about">About</Link>
-          <Link className="link" to="/news">News</Link>
-          <Link className="link" to="/contact">Contact</Link>
+        <i className="fas fa-bars nav_btn" onClick={nav_toggle}></i>
+      <nav className={navToggle? "navBox_2__nav": "navBox_2__nav invi"}>
+          <Link className="link" to="/" onClick={nav_toggle}>Work</Link>
+          <Link className="link" to="/about" onClick={nav_toggle}>About</Link>
+          <Link className="link" to="/news" onClick={nav_toggle}>News</Link>
+          <Link className="link" to="/contact" onClick={nav_toggle}>Contact</Link>
           <a className="link favicon" href="https://www.instagram.com/osangmin_studio/">
             <i className="fab fa-instagram" />
           </a>
       </nav>
+
     </header>
   )
 }
