d5ee7b67c798bc095adaa3f7ff1a926d32556b18
Naked_Friends82 <xkwhdnfdmathfl@hanmail.net>
2021-09-01 21:31:35 +0900

    convert description

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navig from './Navig';
 import Work from '../routes/Work';
 import About from '../routes/About';
