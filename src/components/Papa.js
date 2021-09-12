042b3e682a4682e8d9c8ce96ae2a0c78f616f204
Naked_Friends82 <xkwhdnfdmathfl@hanmail.net>
2021-09-12 13:35:06 +0900

    add space papa description

--- src/components/Papa.js
+++ src/components/Papa.js
@@ -69,8 +69,9 @@ const Papa = () => {
         </div>
         <h1 className="project_header">PAPA <span className="project_year">[2021]</span></h1>
         <p className="project_description">
-          One of Sangmin’s storytelling objects “PAPA” evokes a special memory between him and his father. Alongside his father’s one of the essential rituals at home that is watering the plants, Sangmin designs a watering can which reminds his father that they are together. The design stems from mutual body stretching whenever they went hiking.
-          “My dad came home once a week because of work. I was rarely able to see him due to school when he comes. So, I wanted to give him a special object which makes him recalling our memories. With this watering can, I hope he finds a small pleasure while watering the plants.” says Sangmin.
+          One of Sangmin’s storytelling objects “PAPA” evokes a special memory between him and his father. Alongside his father’s one of the essential rituals at home that is watering the plants, Sangmin designs a watering can which reminds his father that they are together. The design stems from mutual body stretching whenever they went hiking.<br /><br />
+          
+          “My dad came home once a week because of work. I was rarely able to see him due to school when he comes. So, I wanted to give him a special object which makes him recalling our memories. With this watering can, I hope he finds a small happiness while watering the plants.” says Sangmin.
         </p>
         <div className="image_reck">
           <img className="image_reck__imgs" src={pa_mood_RECK[currentMood]} alt="PAPA mood" />
