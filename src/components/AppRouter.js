d7fcde24c22e8b7d592167bc926eebe0eeadc6ee
youngdong82 <nfs82young@gmail.com>
2022-06-03 23:49:22 +0900

    ✨Connect Redux

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -9,24 +9,57 @@ import Footer from './footer';
 import ThanksPressure from '../components/ThanksPressure';
 import Papa from './Papa';
 import Butterfly from './Butterfly';
+//connected-router
+// import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';;
+
+const AppRouter = (props) => {
+  const goToRightUrl = (e) => {
+    const linkSpan = e.target.closest('.link')
+    const url = linkSpan.dataset.url;
+    props.history.push(url);
+  }
+  const goToThankPressure = () => {
+    props.history.push("/work_thankspressure");
+  }
+  const goToPapa = () => {
+    props.history.push("/work_papa");
+  }
+  const goToButterfly = () => {
+    props.history.push("/work_butterfly");
+  }
 
 
-const AppRouter = () => {
-  
-return (
-  <Router>
-    <Navig />
-    <Switch>
-      <Route exact path="/" component={Work} />
-      <Route exact path="/work_thankspressure" component={ThanksPressure} />
-      <Route exact path="/work_papa" component={Papa} />
-      <Route exact path="/work_butterfly" component={Butterfly} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/news" component={News} />
-      <Route exact path="/contact" component={Contact} />
-    </Switch>
-    <Footer />
-  </Router>
+  return (
+    <>
+      <Navig goToRightUrl={goToRightUrl} />
+        <Route exact path="/" 
+          render={() => <Work goToRightUrl={goToRightUrl} />}
+          />
+        <Route exact path="/work_thankspressure" component={ThanksPressure} />
+        <Route exact path="/work_papa" component={Papa} />
+        <Route exact path="/work_butterfly" component={Butterfly} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/news" component={News} />
+        <Route exact path="/contact" component={Contact} />
+      <Footer goToRightUrl={goToRightUrl} />
+    </>
+    // <Router>
+    //   <Navig />
+    //   <Switch>
+    //     <Route exact path="/" component={Work} />
+    //     <Route exact path="/work_thankspressure" component={ThanksPressure} />
+    //     <Route exact path="/work_papa" component={Papa} />
+    //     <Route exact path="/work_butterfly" component={Butterfly} />
+    //     <Route exact path="/about" component={About} />
+    //     <Route exact path="/news" component={News} />
+    //     <Route exact path="/contact" component={Contact} />
+    //   </Switch>
+    //   <Footer />
+    // </Router>
   )
-}
-export default AppRouter;
+};
+
+// export default AppRouter;
+export default connect(null)(withRouter(AppRouter));
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createBrowserHistory } from 'history';
+import { ConnectedRouter } from 'connected-react-router';
+import { configureStore } from './store.js';
+
+//히스토리 객체 반환
+const history = createBrowserHistory();
+const store = configureStore(history);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>
+  </React.StrictMode>
+  , document.getElementById('root')
 );
