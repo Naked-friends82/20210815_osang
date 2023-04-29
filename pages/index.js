5d30e3068289de2c39a4e4df3183a735c5f36719
youngdong82 <nfs82young@gmail.com>
2023-04-29 17:08:20 +0900

    CREATE: Install next dependencies

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import logo from "../public/logo.png";
+
+function App() {
+	return (
+		<main className="App">
+      hello from next js
+		</main>
+	);
+}
+
+export default App;
