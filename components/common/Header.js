8934a1ebbe67c509d7c23495ecc01a54b23a09b6
youngdong82 <nfs82young@gmail.com>
2023-05-02 19:25:22 +0900

    CREATE: apply golbal and header SCSS

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,19 +1,19 @@
 import Link from 'next/link';
 import Image from 'next/image'
 import Navig from './Navig';
-// import "../style/web/navig.css";
-// import "../style/mobile/navig_mobile.css";
+import styles from '../../styles/common/header.module.scss';
 
 const Header = () => {
   return(
-    <header className="navBox">
-      <div className="navBox_1">
-        <Link href='/'>
+    <header className={styles.header}>
+      <div className={styles.logoSection}>
+        <Link href='/' className={styles.mainLogo}>
           <Image 
             src="/asset/logo.png"
-            width={110} 
-            height={20} 
-            alt="osangmain Main Logo" />
+            alt="osangmain Main Logo" 
+            style={{ objectFit: 'cover' }}
+            fill
+            />
         </Link>
       </div>
       <Navig />
--- components/common/Navig.js
+++ components/common/Navig.js
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import { useState } from "react";
 import { AiOutlineInstagram } from 'react-icons/ai';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import styles from '../../styles/common/header.module.scss';
 
 const INSTA_URL = 'https://www.instagram.com/osangmin_studio/';
 
@@ -12,18 +14,21 @@ function Navig(){
   }
 
   return(
-    <div className={navigVisi? "navBox_2__nav" : "navBox_2__nav nav_mobile"}>
-      <div className="forCol">
-        <Link href='/'>Work</Link>
-        <Link href='/about'>About</Link>
-        <Link href='/press'>Press</Link>
-        <Link href='/contact'>Contact</Link>
-        <Link href={INSTA_URL}>
-          <AiOutlineInstagram />
-        </Link>
-        <div className="nav_back" onClick={showNav} />
+    <>
+      <GiHamburgerMenu className={styles.navToggle} />
+      <div className={navigVisi? styles.navSection : `${styles.navSection} ${styles.navMobile}`}>
+        <div className={styles.linkBox}>
+          <Link href='/'>Work</Link>
+          <Link href='/about'>About</Link>
+          <Link href='/press'>Press</Link>
+          <Link href='/contact'>Contact</Link>
+          <Link href={INSTA_URL}>
+            <AiOutlineInstagram className={styles.instaIcon}/>
+          </Link>
+          <div className={styles.backBtn} onClick={showNav} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
