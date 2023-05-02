d7b74f3bb11ed84c594beaf8e73ff60aa3f5a568
youngdong82 <nfs82young@gmail.com>
2023-05-02 19:59:13 +0900

    CREATE: apply contact SCSS

--- pages/contact.js
+++ pages/contact.js
@@ -1,15 +1,14 @@
-// import "../style/web/contact.css"
-// import "../style/mobile/contact_mobile.css"
+import styles from '../styles/contact.module.scss';
 
 const Contact = () => {
 
   return(
-    <section className="section_contact">
-      <div className="contect_description">
-        Tell me your stories and <br className="reremove"/>
+    <section className={styles.section}>
+      <div className={styles.description}>
+        Tell me your stories and <br className={styles.reremove}/>
         please contact for all kinds of collaboration
       </div>
-      <div className="contact_email">
+      <div className={styles.email}>
         Osangminstudio@gmail.com
       </div>
     </section>
