5fa3c028fd81915f7892d6d4b8c9850745cb291b
youngdong82 <nfs82young@gmail.com>
2023-05-05 01:56:24 +0900

    CREATE: apply next/font

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import Layout from '../components/common/Layout';
 import '../styles/common/globals.scss';
+import { Nanum_Myeongjo} from 'next/font/google';
+
+const nanum = Nanum_Myeongjo({
+  subsets: ['latin'],
+  weight: ['400','700']
+})
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <main className={nanum.className}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
-    </>
+    </main>
   )
 }
