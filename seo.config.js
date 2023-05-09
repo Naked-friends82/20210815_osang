32c0853badf5ad11461f50969f3ee4f48cf13a76
youngdong82 <nfs82young@gmail.com>
2023-05-09 20:17:53 +0900

    CREATE: create seo.config --- it works!

---
+++ seo.config.js
@@ -0,0 +1,26 @@
+export default {
+  titleTemplate: '%s - Sangmin Oh',
+  openGraph: {
+    type: 'website',
+    site_name: 'Sangmin Oh',
+    images: [
+      { url: 'https://www.osangmin.com/static/media/knitted_light_1.762fe5533b62db817060.jpg' },
+    ],
+  },
+  additionalLinkTags: [
+    {
+      rel: 'shortcut icon',
+      href: '/favicon.png',
+    },
+  ],
+  // additionalMetaTags: [
+  //   {
+  //     name: 'naver-site-verification',
+  //     content: '03bbee01b695f1b83b82dc27b2aac946fdb31427',
+  //   },
+  //   {
+  //     name: 'google-site-verification',
+  //     content: '_5KMylGBPlSpoGv9goTVZp3wSVur5_JHobernc6pCtw',
+  //   },
+  // ],
+};
