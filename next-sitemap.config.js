9d3539a2393e4925ed0170de826f7caa6764a590
youngdong82 <nfs82young@gmail.com>
2023-05-11 19:58:28 +0900

    ADD: install and apply next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://20210815-osang.vercel.app',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
