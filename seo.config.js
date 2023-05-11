1e3add4e528d2942f13457eb90a45a4af4dd0469
youngdong82 <nfs82young@gmail.com>
2023-05-11 21:42:12 +0900

    EDIT: change og: img url

--- seo.config.js
+++ seo.config.js
@@ -5,7 +5,7 @@ export default {
     site_name: 'Sangmin Oh',
     description: 'Art and design studio established in 2021 by Sangmin Oh based in Netherlands and Korea.',
     images: [
-      { url: 'https://www.osangmin.com/static/media/knitted_light_1.762fe5533b62db817060.jpg' },
+      { url: `${process.env.NEXT_PUBLIC_API_URL}/asset/knitted_light/knitted_light_1.jpg` },
     ],
   },
   additionalLinkTags: [
