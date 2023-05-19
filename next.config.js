dcb84eb0984b967b312428ef920a62c2d4905182
youngdong82 <nfs82young@gmail.com>
2023-05-19 21:24:29 +0900

    ADD: add redirect for previous user

--- next.config.js
+++ next.config.js
@@ -1,5 +1,37 @@
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains: ['osangmin.com'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/work_knitted_light',
+        destination: '/work/knitted_light',
+        permanent: true,
+      },
+      {
+        source: '/work_thankspressure',
+        destination: '/work/thankspressure',
+        permanent: true,
+      },
+      {
+        source: '/work_papa',
+        destination: '/work/papa',
+        permanent: true,
+      },
+      {
+        source: '/work_butterfly',
+        destination: '/work/butterfly',
+        permanent: true,
+      },
+      {
+        source: '/:path',
+        destination: '/',
+        permanent: true,
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
